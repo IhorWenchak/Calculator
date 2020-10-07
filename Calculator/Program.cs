0bf7626521edacdd3e72d015a0a1cf076393a940
Ihor Venchak <iwenchak@gmail.com>
2020-10-07 13:23:17 +0300

    Simple mode

---
+++ Calculator/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Calculator
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine(" 1 - User input in the console of simple operations (without brackets).");
+			Console.WriteLine(" 2 - Reading from a file line by line expressions (to implement the calculation of expressions with brackets):");
+			Console.WriteLine();
+			Console.Write(" Select the calculator operating mode: ");
+
+			string alegereMode = Console.ReadLine();
+
+			if (int.TryParse(alegereMode, out int valMode))
+			{
+				switch (valMode)
+				{
+					case 1:
+
+						string inputLine = String.Empty;
+
+						string outputLine = String.Empty;
+
+						do
+						{
+							Console.ForegroundColor = ConsoleColor.Red;
+							Console.WriteLine(String.Format("{0}\n", outputLine));
+							Console.ForegroundColor = ConsoleColor.White;
+							Console.Write("Enter numbers and simple operations (without brackets !). Try now: ");
+							inputLine = Console.ReadLine();
+							outputLine = SimpleMode.IsCorrectlyInput(inputLine);
+						} while (outputLine != String.Empty);
+
+						SimpleMode simple = new SimpleMode(inputLine);
+						
+						Console.ForegroundColor = ConsoleColor.Cyan;
+						Console.WriteLine(String.Format("Your result: {0}", simple.GetResult()));
+
+						break;
+
+					case 2:
+						Console.WriteLine("Reading from a file line by line expressions (to implement the calculation of expressions with brackets)");
+						break;
+					
+					default:
+						Console.ForegroundColor = ConsoleColor.Red;
+						Console.WriteLine("This mode does not exist.");
+						break;
+				}
+			}
+
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Not a valid symbol.");
+			}
+			Console.ReadKey();
+		}
+	}
+}
---
+++ Calculator/SimpleMode.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading;
+
+namespace Calculator
+{
+
+	public class SimpleMode
+	{
+		private string _enteredString = String.Empty;		
+		private readonly char[] _startOperations = new char[] { '+', '-' };
+		private readonly char[] _priorityOperations = new char[] { '*', '/', '%', ':' };
+		private static readonly char[] _allOperations = new char[] { '*', '/', '%', '+', '-', ':' };
+		
+		public SimpleMode(string enteredString)
+		{
+			_enteredString = enteredString;
+		}
+	
+		public double GetResult()
+		{
+			int counter = 0;
+
+			string[] devidedInput = _enteredString.Split(_startOperations);
+
+			double total = GetPriorityValue(devidedInput[counter]);
+
+			IEnumerable<char> operations =  _enteredString.Where(symbol => _startOperations.Contains(symbol));
+
+			foreach (char symbol in operations)
+			{
+				counter++;
+				double addition = GetPriorityValue(devidedInput[counter]);
+				total = (symbol == '+') ? (total + addition) : (total - addition);
+			}
+			return total;
+		}
+
+		public double GetPriorityValue(string divided)
+		{
+			int counter = 0;
+
+			string[] listNumbers = divided.Split(_priorityOperations);
+
+			double.TryParse(listNumbers[counter], out double number);
+
+			double thisTotal = number;
+
+			if (listNumbers.Length == 1)
+			{
+				return thisTotal;
+			}
+
+			IEnumerable<char> operations = divided.Where(symbol => _priorityOperations.Contains(symbol));
+
+			foreach (char operand in operations)
+				{
+					counter++;
+
+					double.TryParse(listNumbers[counter], out number);
+
+					switch (operand)
+					{
+						case '*':
+							thisTotal *= number;
+							break;
+						case '/':
+							thisTotal /= number;
+							break;
+						case ':':
+							thisTotal /= number;
+							break;
+						case '%':
+							thisTotal %=  number;
+							break;
+					}
+				}
+
+			return thisTotal;
+		}
+
+
+		public static string IsCorrectlyInput(string inputRow)
+		{
+			int counter = 0;
+
+			string[] digitalsList = inputRow.Split(_allOperations);
+
+			IEnumerable<char> operations = inputRow.Where(symbol => _allOperations.Contains(symbol));
+
+			if (String.IsNullOrEmpty(inputRow))
+			
+				return "This data is empty.";
+			
+			else if (inputRow.Contains('(') | inputRow.Contains(')'))
+			
+				return "Without brackets !";			
+			
+			foreach (string digital in digitalsList)
+			{
+				if (!double.TryParse(digital, NumberStyles.Number, CultureInfo.CurrentCulture, out double number))
+					
+						return String.Format("Сan't digitize: {0} number decimal separator is {1} ", digital, CultureInfo.CurrentCulture.NumberFormat.NumberDecimalSeparator);
+			}
+
+			foreach (char symbol in operations)
+			{
+				counter++;
+
+				if (((symbol == '/') || (symbol == ':') || (symbol == '%')) && (double.TryParse(digitalsList[counter], out double number)) && (number == 0))
+		
+					return "It is forbidden to divide by zero !";
+			}
+
+			return String.Empty;
+		}
+
+	}
+}	
+
